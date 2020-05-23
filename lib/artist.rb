1eb73e5c7873772f6e5547d8131925a4ab13c284
Aurora Avery <aurora.avery@yahoo.com>
2020-05-23 09:33:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,34 @@
+class Artist
+  attr_accessor :name
+  attr_reader :songs
 
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
+  @@artists = []
+
+  def initialize
+    @@artists << self
+    @songs = []
+  end
+
+  def self.find_by_name(name)
+    @@artists.detect{|a| a.name == name}
+  end
+
+  def self.all
+    @@artists
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self unless song.artist
+  end
+
+  def add_songs(songs)
+    songs.each { |song| add_song(song) }
+  end
+
+end
